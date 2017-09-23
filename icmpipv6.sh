2aa2ebe146215b5c24523cba1a5436a3502a3e04
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-09-23 09:18:32 +0200

    Update icmpipv6.sh

--- icmpipv6.sh
+++ icmpipv6.sh
@@ -3,10 +3,10 @@
 # https://es.wikipedia.org/wiki/ICMPv6
 # ndisc6 opcion IPv6 eth0
 # https://linux.die.net/man/8/ndisc6
-# rltraceroute6 IPv6
-# ICMPv6 neighbor discovery requires two types of ICMPv6
-# ICMPv6 Neighbor solicitation (ICMPv6 Type 135) 
+# ICMPv6 neighbor discovery requires two types of ICMPv6.
+# ICMPv6 Neighbor solicitation (ICMPv6 Type 135). 
 # ICMPv6 neighbor advertisement (ICMPv6 type 136).
+# rltraceroute6 IPv6.
 # tcptraceroute6, a TCP/IPv6-based traceroute implementation,
 # tracert6, a ICMPv6 Echo Request based traceroute,
 # tcpspray6, a TCP/IP Discard/Echo bandwidth metter.
