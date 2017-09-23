4d0ce87230c4e530c269fef8091f565c7931060f
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-09-23 09:15:37 +0200

    Update icmpipv6.sh

--- icmpipv6.sh
+++ icmpipv6.sh
@@ -7,6 +7,9 @@
 # ICMPv6 neighbor discovery requires two types of ICMPv6
 # ICMPv6 Neighbor solicitation (ICMPv6 Type 135) 
 # ICMPv6 neighbor advertisement (ICMPv6 type 136).
+# tcptraceroute6, a TCP/IPv6-based traceroute implementation,
+# tracert6, a ICMPv6 Echo Request based traceroute,
+# tcpspray6, a TCP/IP Discard/Echo bandwidth metter.
 echo
 echo "...Script para envio de n Icmp IPv6 Neigbord Discovery" 
 echo "Uso: ./icmpipv6.sh IPv6 Num_peticiones"
