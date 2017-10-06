d9d8c6e0404b5117004674449737cae72c56563a
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-10-06 22:42:58 +0200

    Update icmpipv6.sh

--- icmpipv6.sh
+++ icmpipv6.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-# https://es.wikipedia.org/wiki/Neighbor_Discovery
-# https://es.wikipedia.org/wiki/ICMPv6
-# ndisc6 opcion IPv6 eth0
-# https://linux.die.net/man/8/ndisc6
-# ICMPv6 neighbor discovery requires two types of ICMPv6.
-# ICMPv6 Neighbor solicitation (ICMPv6 Type 135). 
-# ICMPv6 neighbor advertisement (ICMPv6 type 136).
-# rltraceroute6 IPv6.
-# tcptraceroute6, a TCP/IPv6-based traceroute implementation,
-# tracert6, a ICMPv6 Echo Request based traceroute,
-# tcpspray6, a TCP/IP Discard/Echo bandwidth metter.
 echo
-echo "...Script para envio de n Icmp IPv6" 
+echo "...Script para envio de ping Icmp IPv6" 
 echo "Uso: ./icmpipv6.sh IPv6 Num_peticiones"
+echo "Por ejemplo google.com 2a00:1450:4003:801::200e"
 for i in 1 `seq 1 $2`
 do
-echo $i
 ping6 $1
 done
