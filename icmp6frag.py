740ed5ebd1ffc41e518c656ec5011f8754faa828
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-10-06 22:10:17 +0200

    Update and rename icmp6.py to icmp6frag.py

--- icmp6frag.py
+++ icmp6frag.py
@@ -2,6 +2,6 @@
 # (c) hackingyseguridad.com 2017
 from scapy.all import *
 sip = 'fe80::20c:29ff:fe67:22c2'
-dip = 'fe80::700d:da7e:80bb:aca9'
+dip = '2a00:1450:4003:801::200e'
 packets = fragment6(IPv6(src=sip, dst=dip) / IPv6ExtHdrFragment() / ICMPv6EchoRequest(data='A'*5000), 1024)
 send(packets)
