63a5f90be68ba7d36e2d7f38ca230a425e0fc882
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-10-06 22:52:55 +0200

    Update icmpipv6.sh

--- icmpipv6.sh
+++ icmpipv6.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 echo
 echo "...Script para envio de ping Icmp IPv6" 
-echo "Uso: ./icmpipv6.sh IPv6 Num_peticiones"
-echo "Por ejemplo google.com 2a00:1450:4003:801::200e"
-for i in 1 `seq 1 $2`
-do
-ping6 $1
-done
+echo "Por ejemplo IPv6 google.com 2a00:1450:4003:801::200e"
+ping6 -c2 -I eth0 2a00:1450:4003:801::200e && echo -e "\nIPv6 Neighbours:\n" && ip -6 neigh
